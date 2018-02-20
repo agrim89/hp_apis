ac1e4f04ab17e60b071ce8ebc1001608f644fdeb
encode <encode@encode.91.90-234.primenet.in>
2018-02-20 13:18:16 +0530

    settings

--- hp_backend/hp_backend/settings.py
+++ hp_backend/hp_backend/settings.py
@@ -76,15 +76,24 @@ WSGI_APPLICATION = 'hp_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hp_data',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
-
-# Password validation
+## Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [

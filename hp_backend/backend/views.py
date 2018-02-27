9e0d2245ae60fcdbcfcd79efb6d0c458953c2961
agrim <agrim.sharma@sirez.com>
2018-02-27 20:07:42 +0530

    updated

--- hp_backend/backend/views.py
+++ hp_backend/backend/views.py
@@ -210,14 +210,18 @@ def report_api(request):
     report_deactive = ReportGenerator('deactive_user_report_{}.xlsx'.format(datetime.datetime.now().date()))
     now = datetime.datetime.now().date()
     last = now - datetime.timedelta(days=30)
-    active_user = PartnerSalesTeam.objects.filter(last_login__gte=last)
-    deactive_user = PartnerSalesTeam.objects.filter(last_login__lte=last)
-    active = user_data(active_user)
-    deactive = user_data(deactive_user)
+    active_user = PartnerSalesTeam.objects.filter(last_login__gte=last).values('first_name', 'email',
+                                                                               'dealer_name__company_name',
+                                                                               'login_count', 'last_login')
+    deactive_user = PartnerSalesTeam.objects.filter(last_login__lte=last).values('first_name', 'email',
+                                                                               'dealer_name__company_name',
+                                                                               'login_count', 'last_login')
+    # active = user_data(active_user)
+    # deactive = user_data(deactive_user)
     report_active.write_header(col_heads)
     report_deactive.write_header(col_heads)
-    report_active.write_body(active)
-    report_deactive.write_body(deactive)
+    report_active.write_body(active_user)
+    report_deactive.write_body(deactive_user)
     mail = EmailMessage('subject', 'text', 'agrim.sharma@sirez.com', ["agrim.sharma@sirez.com"])
     mail.attach_file(os.path.join(settings.STATIC_ROOT, 'active_user_report_{}.xlsx'.\
                                   format(datetime.datetime.now().date())))
@@ -255,7 +259,7 @@ def user_data(user):
         i = 1
         name = a.get_full_name()
         email = a.email
-        dealer_name = a.dealer_name.company_name.decode('utf-8')
+        dealer_name = a.dealer_name.company_name
         login_count = a.login_count
         last_login = datetime.datetime.strftime(a.last_login, "%Y-%m-%d")
         data.append([i, name, email, dealer_name, login_count, last_login])

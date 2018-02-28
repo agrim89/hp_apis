c37acbcaae2b4c8687aa1c4e01d94eea00c9bf1d
agrim <agrim.sharma@sirez.com>
2018-02-28 18:18:36 +0530

    updated

--- hp_backend/backend/views.py
+++ hp_backend/backend/views.py
@@ -10,8 +10,6 @@ from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import EmailMultiAlternatives
-from django.template.loader import get_template
-from django.template import Context
 
 
 class UserDetail(APIView):
@@ -293,20 +291,20 @@ def send_email(request):
         #                                     ))
         #                     )
 
-        # html_content = render_to_string('mail_template.html', {"yest": datetime.datetime.strftime(yest, '%Y-%m-%d'),
-        #                                                        "dyest": datetime.datetime.strftime(dyes, '%Y-%m-%d'),
-        #                                                        "nosdyes": login_dyest,
-        #                                                        "uns_dyes": unique_dyes,
-        #                                                        "nosyes": login_yest,
-        #                                                        "pcn": pcn,
-        #                                                        "pchn": pchn,
-        #                                                        "uns_yes": unique_yes
-        #                                                        })
-        # text_content = strip_tags(html_content)
+        html_content = render_to_string('mail_template.html', {"yest": datetime.datetime.strftime(yest, '%Y-%m-%d'),
+                                                               "dyest": datetime.datetime.strftime(dyes, '%Y-%m-%d'),
+                                                               "nosdyes": login_dyest,
+                                                               "uns_dyes": unique_dyes,
+                                                               "nosyes": login_yest,
+                                                               "pcn": pcn,
+                                                               "pchn": pchn,
+                                                               "uns_yes": unique_yes
+                                                               })
+        text_content = strip_tags(html_content)
         # # # msg = EmailMessage('Test', 'text_content', to=['agrim.sharma@sirez.com'])
-        # msg = EmailMultiAlternatives('Test', text_content, 'agrim.sharma@sirez.com', ['agrim.sharma@sirez.com'])
-        # msg.attach_alternative(html_content, "text/html")
-        # msg.send()
+        msg = EmailMultiAlternatives('Test', text_content, 'agrim.sharma@sirez.com', ['agrim.sharma@sirez.com',])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
         return HttpResponse(json.dumps(dict(payload={"yest": datetime.datetime.strftime(yest, '%Y-%m-%d'),
                                                                "dyest": datetime.datetime.strftime(dyes, '%Y-%m-%d'),
                                                                "nosdyes": login_dyest,
